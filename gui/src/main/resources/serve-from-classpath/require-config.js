b5a7016c3a35b0e3d90c85515b29e64f4222558b
SeanShubin <seanshubin@gmail.com>
2014-05-15 19:21:49 -0700

    Split todo into test and implementation

--- gui/src/main/resources/serve-from-classpath/require-config.js
+++ gui/src/main/resources/serve-from-classpath/require-config.js
@@ -1,6 +1,18 @@
 var require = {
     baseUrl: '.',
     paths: {
-        'jquery': 'lib/jquery-2.1.1'
+        'jquery': 'lib/jquery-2.1.1',
+        'backbone': 'lib/backbone',
+        'underscore': 'lib/underscore',
+        'qunit': 'lib/qunit-1.14.0'
+    },
+    shim: {
+        'qunit': {
+            exports: 'QUnit',
+            init: function() {
+                QUnit.config.autoload = false;
+                QUnit.config.autostart = false;
+            }
+        }
     }
 };
---
+++ gui/src/main/resources/serve-from-classpath/todo.js
@@ -0,0 +1,59 @@
+define(['backbone'], function(Backbone) {
+    function createTodoApplication() {
+        var ListView, listView, List, Model, View;
+
+        Model = Backbone.Model.extend({
+            defaults: {
+                name: 'item',
+                number: 0
+            }
+        });
+
+        View = Backbone.View.extend({
+            tagName: 'li',
+            render: function () {
+                this.$el.append(this.model.get('name') + ' ' + this.model.get('number'));
+                return this;
+            }
+        });
+
+        List = Backbone.Collection.extend({
+            model: Model
+        });
+
+        ListView = Backbone.View.extend({
+            events: {
+                'click button.add': 'addItemToModel'
+            },
+            initialize: function () {
+                this.counter = 1;
+                this.collection = new List();
+                this.collection.bind('add', this.addItemToView, this);
+            },
+            render: function () {
+                this.$el.append('<button class="add">Add list item</button>');
+                this.$el.append('<ul></ul>');
+                return this;
+            },
+            addItemToModel: function () {
+                var item = new Model();
+                item.set({
+                    number: this.counter
+                });
+                this.counter++;
+                this.collection.add(item);
+            },
+            addItemToView: function (model) {
+                var view = new View({
+                    model: model
+                });
+                this.$('ul').append(view.render().el);
+            }
+        });
+
+        listView = new ListView();
+        return listView.render().$el;
+    }
+
+    return createTodoApplication;
+});
