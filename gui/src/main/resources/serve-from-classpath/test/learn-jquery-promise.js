da4d4e9d2891e7af247eaa33e0d9901815899624
Sean Shubin <seanshubin@gmail.com>
2014-05-21 13:55:58 -0700

    Added sample for jquery promise

---
+++ gui/src/main/resources/serve-from-classpath/test/learn-jquery-promise.js
@@ -0,0 +1,56 @@
+define(['qunit', 'jquery'], function (qunit, $) {
+    'use strict';
+    qunit.module('learn-jquery-promise');
+    qunit.asyncTest('simple promise', function () {
+        var  deferred, assertResult;
+        assertResult = function (x) {
+            qunit.equal(x, 3, 'x equals 3');
+            qunit.start();
+        };
+        deferred = $.Deferred();
+        deferred.then(assertResult);
+        deferred.resolve(3);
+    });
+    qunit.asyncTest('convert callback to promise', function () {
+        var deferred, promise, millisecondsToWait;
+        qunit.expect(1);
+        millisecondsToWait = 1;
+        deferred = $.Deferred();
+        setTimeout(function () {
+            deferred.resolve(3)
+        }, millisecondsToWait);
+        promise = deferred.promise();
+        promise.then(function (value) {
+            qunit.equal(value, 3, 'x equals 3');
+            qunit.start();
+        });
+    });
+    qunit.asyncTest('join two promises in serial', function () {
+        var returnThree, multiplyByTwo, assertResult;
+        returnThree = $.Deferred();
+        multiplyByTwo = function (x) {
+            return x * 2;
+        };
+        assertResult = function (x) {
+            qunit.equal(x, 6, 'x equals 6');
+            qunit.start();
+        };
+        $.when(returnThree).then(multiplyByTwo).then(assertResult);
+        returnThree.resolve(3);
+    });
+    qunit.asyncTest('join two promises in parallel', function () {
+        var returnTwo, returnThree, multiply, assertResult;
+        returnTwo = $.Deferred();
+        returnThree = $.Deferred();
+        multiply = function (x, y) {
+            return x * y;
+        };
+        assertResult = function (x) {
+            qunit.equal(x, 6, 'x equals 6');
+            qunit.start();
+        };
+        $.when(returnTwo, returnThree).then(multiply).then(assertResult);
+        returnTwo.resolve(2);
+        returnThree.resolve(3);
+    });
+});
