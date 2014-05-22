0d99cc86e53bace8ba478d21f21a460d7134744c
Sean Shubin <seanshubin@gmail.com>
2014-05-21 17:52:03 -0700

    refactored latest attempt

--- gui/src/main/resources/serve-from-classpath/todo/sean/latest-attempt/createTodoApplication.js
+++ gui/src/main/resources/serve-from-classpath/todo/sean/latest-attempt/createTodoApplication.js
@@ -1,31 +1,30 @@
 define(['jquery', 'underscore'], function ($, _) {
     'use strict';
     function createTodoApplication(jsonOverHttp) {
-        var dom, addItemToModel, counter, addButton, list, addItemToView, addItemResponseToView, setItemsInView, response;
+        var dom, sendAddItemRequest, counter, addButton, list, appendItemToView, respondToItemAdded, respondToRefreshItems;
         dom = $("<div></div>");
         addButton = dom.append('<button class="add">Add list item</button>');
         list = dom.append('<ul></ul>');
         counter = 1;
-        addItemToModel = function () {
+        sendAddItemRequest = function () {
             var item;
             item = {name: 'item', number: counter};
             counter++;
-            jsonOverHttp({uri: 'item', method: 'POST', body: item}).then(addItemResponseToView);
+            jsonOverHttp({uri: 'item', method: 'POST', body: item}).then(respondToItemAdded);
         };
-        addItemToView = function (item) {
+        appendItemToView = function (item) {
             var text;
             text = '<li>' + item.name + ' ' + item.number + '</li>';
             list.append($(text));
         };
-        addItemResponseToView = function (response) {
-            addItemToView(response.body);
+        respondToItemAdded = function (response) {
+            appendItemToView(response.body);
         };
-        setItemsInView = function (response) {
-            _.each(response.body, addItemToView);
+        respondToRefreshItems = function (response) {
+            _.each(response.body, appendItemToView);
         };
-        addButton.on('click', addItemToModel);
-        response = jsonOverHttp({uri: 'item', method: 'GET'});
-        response.then(setItemsInView);
+        addButton.on('click', sendAddItemRequest);
+        jsonOverHttp({uri: 'item', method: 'GET'}).then(respondToRefreshItems);
         return dom;
     }
 
