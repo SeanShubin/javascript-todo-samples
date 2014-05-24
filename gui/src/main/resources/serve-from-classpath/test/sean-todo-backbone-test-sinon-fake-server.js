7c72d4775c9d3d9449944b43d36e2ac150662f2c
SeanShubin <seanshubin@gmail.com>
2014-05-24 00:42:30 -0700

    Fixed false positive with Backbone fake server

--- gui/src/main/resources/serve-from-classpath/test/sean-todo-backbone-test-sinon-fake-server.js
+++ gui/src/main/resources/serve-from-classpath/test/sean-todo-backbone-test-sinon-fake-server.js
@@ -7,16 +7,18 @@ define(['lib/domReady!',
     'use strict';
     qunit.module('sean-todo-backbone-test-sinon-fake-server');
     qunit.test('start with no items', function () {
-        var $el, server, responseString;
-        responseString = JSON.stringify([]);
+        var $el, server;
         server = sinon.fakeServer.create();
+
         $el = createTodoApplication();
-        server.respondWith('GET', 'item', [200, { 'Content-Type': 'application/json' }, responseString]);
+        server.respondWith('GET', 'item', [200, { 'Content-Type': 'application/json' }, JSON.stringify([])]);
         server.respond();
+
         qunit.equal(server.requests.length, 1, 'server.requests.length');
         qunit.equal(server.requests[0].method, 'GET', 'server.requests[0].method ' + server.requests[0].method);
         qunit.equal(server.requests[0].url, 'item', 'server.requests[0].url ' + server.requests[0].url);
         qunit.equal($el.find('li').length, 0, 'no items');
+
         server.restore();
     });
     qunit.test('add item', function () {
@@ -28,13 +30,10 @@ define(['lib/domReady!',
         server.respond();
 
         $el.find('.add').click();
-        server.respondWith('POST', 'item', [201, { 'Content-Type': 'application/json' }, JSON.stringify([
-            {'number': 1, 'name': 'item', 'id': '1'}
-        ])]);
-        server.respond();
 
         qunit.equal($el.find('li').length, 1, 'one item added');
         qunit.equal($($el.find('li')[0]).text(), 'item 1', 'item 1');
+
         server.restore();
     });
     qunit.test('add many items', function () {
@@ -46,29 +45,39 @@ define(['lib/domReady!',
         server.respond();
 
         addButton = $el.find('.add');
-
         addButton.click();
-        server.respondWith('POST', 'item', [201, { 'Content-Type': 'application/json' }, JSON.stringify([
-            {'number': 1, 'name': 'item', 'id': '1'}
-        ])]);
-        server.respond();
-
         addButton.click();
-        server.respondWith('POST', 'item', [201, { 'Content-Type': 'application/json' }, JSON.stringify([
-            {'number': 2, 'name': 'item', 'id': '2'}
-        ])]);
-        server.respond();
-
         addButton.click();
-        server.respondWith('POST', 'item', [201, { 'Content-Type': 'application/json' }, JSON.stringify([
-            {'number': 3, 'name': 'item', 'id': '3'}
-        ])]);
+
+        qunit.equal($el.find('li').length, 3, 'three items added');
+        qunit.equal($($el.find('li')[0]).text(), 'item 1', 'item 1');
+        qunit.equal($($el.find('li')[1]).text(), 'item 2', 'item 2');
+        qunit.equal($($el.find('li')[2]).text(), 'item 3', 'item 3');
+
+        server.restore();
+    });
+    qunit.test('start with many items', function () {
+        var $el, server;
+        server = sinon.fakeServer.create();
+
+        $el = createTodoApplication();
+        server.respondWith('GET', 'item',
+            [
+                200,
+                { 'Content-Type': 'application/json' },
+                JSON.stringify([
+                    { id: "1", name: "item", number: 1 },
+                    { id: "2", name: "item", number: 2 },
+                    { id: "3", name: "item", number: 3 }
+                ])
+            ]);
         server.respond();
 
         qunit.equal($el.find('li').length, 3, 'three items added');
         qunit.equal($($el.find('li')[0]).text(), 'item 1', 'item 1');
         qunit.equal($($el.find('li')[1]).text(), 'item 2', 'item 2');
         qunit.equal($($el.find('li')[2]).text(), 'item 3', 'item 3');
+
         server.restore();
     });
 });
