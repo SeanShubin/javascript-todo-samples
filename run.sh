b3145a44182c950b03ffad95fd7bd5ef7662d95c
SeanShubin <seanshubin@gmail.com>
2014-05-26 11:55:18 -0700

    Tried to get htmlunit working
    Didn’t work, it is incompatible with jetty 9

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar server/target/server.jar 4000 gui/src/main/resources/
--- test/src/test/scala/com/seanshubin/todo/sample/test/JavascriptUnitTests.scala
+++ test/src/test/scala/com/seanshubin/todo/sample/test/JavascriptUnitTests.scala
@@ -3,20 +3,25 @@ package com.seanshubin.todo.sample.test
 import org.scalatest.FunSuite
 import com.seanshubin.todo.sample.server.{Configuration, RunnerWiring}
 import com.seanshubin.todo.sample.client.HttpImpl
-import com.seanshubin.todo.sample.core.http.{Content, Client}
+import com.seanshubin.todo.sample.core.http.Client
+//import com.gargoylesoftware.htmlunit.WebClient
+//import com.gargoylesoftware.htmlunit.html.HtmlPage
 
 class JavascriptUnitTests extends FunSuite {
   test("javascript unit tests") {
-    val runner = new RunnerWiring(){
+    val runner = new RunnerWiring() {
       lazy val freePortFinder: FreePortFinder = new FreePortFinderImpl
       lazy val port = freePortFinder.port
       lazy val maybeServeFromPath: Option[String] = None
-      lazy val configuration:Configuration = Configuration(port, maybeServeFromPath)
-      lazy val client:Client = new HttpImpl
+      lazy val configuration: Configuration = Configuration(port, maybeServeFromPath)
+      lazy val client: Client = new HttpImpl
     }
     runner.jettyServer.start()
     val response = runner.client.send("localhost", runner.port, "get", "/all-tests.html")
     println(response.content)
+    //    val webClient:WebClient = new WebClient()
+    //    val htmlPage:HtmlPage = webClient.getPage(s"http://localhost:${runner.port}/all-tests.html")
+    //    println(htmlPage)
     runner.jettyServer.stop()
   }
 }
