3b258884a580cc1dac5c73925ecd6121cf261f8c
SeanShubin <seanshubin@gmail.com>
2014-05-26 20:55:16 -0700

    Formatted source

--- core/src/main/scala/com/seanshubin/todo/sample/core/OrderedMap.scala
+++ core/src/main/scala/com/seanshubin/todo/sample/core/OrderedMap.scala
@@ -4,21 +4,21 @@ case class OrderedMap[KeyType, ValueType](keys: Seq[KeyType] = Seq[KeyType](),
                                           map: Map[KeyType, ValueType] = Map[KeyType, ValueType]()) {
   def +(entry: (KeyType, ValueType)) = {
     val (key, _) = entry
-    val newOrder = if(keys.contains(key)) keys else keys :+ key
+    val newOrder = if (keys.contains(key)) keys else keys :+ key
     val newMap = map + entry
     OrderedMap(newOrder, newMap)
   }
 
-  def -(key:KeyType) = {
+  def -(key: KeyType) = {
     val newKeys = keys.filter(_ != key)
     val newMap = map - key
     OrderedMap(newKeys, newMap)
   }
 
-  def updated(key:KeyType, value:ValueType) = this + (key, value)
+  def updated(key: KeyType, value: ValueType) = this +(key, value)
 
-  def apply(key:KeyType) = map(key)
-  
+  def apply(key: KeyType) = map(key)
+
   def keyToEntry(key: KeyType): (KeyType, ValueType) = (key, map(key))
 
   def toSeq: Seq[(KeyType, ValueType)] = keys.map(keyToEntry)
