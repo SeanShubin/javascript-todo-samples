da285067853895d8e81c1a287b4daf8659c0a3ce
Sean Shubin <seanshubin@gmail.com>
2014-05-28 17:43:14 -0700

    Refactored duplication

--- gui/src/main/resources/serve-from-classpath/todo/sean/full-featured/createTodoComponent.js
+++ gui/src/main/resources/serve-from-classpath/todo/sean/full-featured/createTodoComponent.js
@@ -6,7 +6,7 @@ define(['jquery',
     'use strict';
     function createPrototypeTodoComponent(jsonOverHttp) {
         var dom, addButtonPressed, keyPressed, addButton, userInput, list, appendTodoEntryToView, respondToTodoAdded,
-            respondToRefreshTodoEntries, respondToTodoDeleted, respondToTodoModified;
+            respondToRefreshTodoEntries, respondToTodoDeleted, respondToTodoModified, updateTodoElementValues;
         dom = $(pageTemplate);
         addButton = dom.find('.add-todo-entry-button');
         userInput = dom.find('.user-input');
@@ -29,14 +29,9 @@ define(['jquery',
         appendTodoEntryToView = function (todoEntry) {
             var todoElement, deleteEvent, doneEvent;
             todoElement = $(todoEntryTemplate);
+            list.append(todoElement);
             todoElement.addClass('todo-id-' + todoEntry.id);
-            todoElement.find('.todo-id').val(todoEntry.id);
-            todoElement.find('.todo-name').text(todoEntry.name);
-            if (todoEntry.done) {
-                todoElement.find('.todo-done').attr('checked', 'checked');
-            } else {
-                todoElement.find('.todo-done').removeAttr('checked');
-            }
+            updateTodoElementValues(todoEntry);
             deleteEvent = function () {
                 jsonOverHttp({uri: 'todo-entry/' + todoEntry.id, method: 'DELETE'}).then(respondToTodoDeleted);
             };
@@ -50,7 +45,6 @@ define(['jquery',
             };
             todoElement.find('.todo-delete').on('click', deleteEvent);
             todoElement.find('.todo-done').on('change', doneEvent);
-            list.append(todoElement);
         };
         respondToTodoAdded = function (response) {
             appendTodoEntryToView(response.body);
@@ -61,14 +55,17 @@ define(['jquery',
         respondToTodoDeleted = function (response) {
             dom.find('.todo-id-' + response.body.id).remove();
         };
-        respondToTodoModified = function (response) {
-            var todoElement = dom.find('.todo-id-' + response.body.id);
-            todoElement.find('.todo-name').text(response.body.name);
-            if (response.body.done) {
+        updateTodoElementValues = function(todoEntry) {
+            var todoElement = dom.find('.todo-id-' + todoEntry.id);
+            todoElement.find('.todo-name').text(todoEntry.name);
+            if (todoEntry.done) {
                 todoElement.find('.todo-done').attr('checked', 'checked');
             } else {
                 todoElement.find('.todo-done').removeAttr('checked');
             }
+        };
+        respondToTodoModified = function (response) {
+            updateTodoElementValues(response.body);
         };
         addButton.on('click', addButtonPressed);
         userInput.on('keyup', keyPressed);
