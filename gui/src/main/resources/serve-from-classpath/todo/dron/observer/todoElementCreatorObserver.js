a2ed069f08f6f6a91616a24122334f78b14a4cd1
David Ron <dron@cj.com>
2014-06-13 14:27:06 -0700

    With great regret and sadness, I mourn the loss of my beloved never-undefined variables in the name of easier debugging.

--- gui/src/main/resources/serve-from-classpath/todo/dron/observer/todoElementCreatorObserver.js
+++ gui/src/main/resources/serve-from-classpath/todo/dron/observer/todoElementCreatorObserver.js
@@ -3,6 +3,7 @@ define([
         "http/json-over-http"
     ],function(templateService, jsonOverHttp){
         function plugin(element){
+
             element.find("form").on("submit",function(event){
                 var newItemName=element.find("input.new-todo").val();
                 jsonOverHttp({uri: '/db/item', method:'POST', body:{name:newItemName}}).then(function(){
@@ -15,6 +16,5 @@ define([
         return {
             observer:templateService.observer({method:plugin, elementLocator:"[data-todo-creator]"}),
             setJsonOverHttp:function(j){jsonOverHttp = j;}
-        };
-        
+        };        
 });
--- gui/src/main/resources/serve-from-classpath/todo/dron/observer/todoElementObserver.js
+++ gui/src/main/resources/serve-from-classpath/todo/dron/observer/todoElementObserver.js
@@ -1,14 +1,21 @@
 define(["jquery", "lib/templateobserver", "http/json-over-http"], function ($, templateService, jsonOverHttp) {
     function plugin(element){
-        var todoName = element.data("todo-name"),
-            todoId = element.data("todo-id"),
-            todoIsDone = element.hasClass("todo-done"),
-            checkbox = $("<input type=checkbox>").prop("checked", todoIsDone).click(function(){
-                var newState = todoIsDone ? false:true;
-                jsonOverHttp({uri: '/db/item/'+todoId, method:'PATCH', body:{done:newState, id:todoId}}).then(function(){
-                    element.trigger("todoStateChange", newState);
-                });
+        var todoName,
+            todoId,
+            todoIsDone,
+            checkbox;
+
+        todoName = element.data("todo-name");
+        todoId = element.data("todo-id");
+        todoIsDone = element.hasClass("todo-done");
+
+        checkbox = $("<input type=checkbox>").prop("checked", todoIsDone).click(function(){
+            var newState = todoIsDone ? false:true;
+            jsonOverHttp({uri: '/db/item/'+todoId, method:'PATCH', body:{done:newState, id:todoId}}).then(function(){
+                element.trigger("todoStateChange", newState);
             });
+        });
+
         element.append(checkbox).append("<span>"+todoName+"</span>");
 
         element.on("todoClearCompleted", function(){
