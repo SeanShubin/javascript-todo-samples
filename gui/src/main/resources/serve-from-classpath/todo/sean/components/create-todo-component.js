0f1dab552854d4a52378ca407ebde3abae195a1d
Sean Shubin <seanshubin@gmail.com>
2014-06-24 17:33:20 -0700

    More documentation for sean's samples

--- gui/src/main/resources/serve-from-classpath/todo/sean/components/create-todo-component.js
+++ gui/src/main/resources/serve-from-classpath/todo/sean/components/create-todo-component.js
@@ -1,3 +1,22 @@
+define(['todo/sean/components/layout',
+        'todo/sean/components/data-access'],
+    function (createLayout, createDataAccess) {
+        'use strict';
+        return function (jsonOverHttp) {
+            var dataAccess, layout, initialize, component;
+            dataAccess = createDataAccess(jsonOverHttp);
+            initialize = function () {
+                dataAccess.getAndFireCreateForAll();
+                return component;
+            };
+            layout = createLayout(dataAccess);
+            component = {
+                dom: layout,
+                initialize: initialize
+            };
+            return component;
+        };
+    });
 /*
  Terminology guide
  model
@@ -21,22 +40,3 @@
  -   delete
  -   get
  */
-define(['todo/sean/components/layout',
-        'todo/sean/components/data-access'],
-    function (createLayout, createDataAccess) {
-        'use strict';
-        return function (jsonOverHttp) {
-            var dataAccess, layout, initialize, component;
-            dataAccess = createDataAccess(jsonOverHttp);
-            initialize = function () {
-                dataAccess.getAndFireCreateForAll();
-                return component;
-            };
-            layout = createLayout(dataAccess);
-            component = {
-                dom: layout,
-                initialize: initialize
-            };
-            return component;
-        };
-    });
