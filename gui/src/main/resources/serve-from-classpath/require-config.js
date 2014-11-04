f7fc20faae72cf67cc91f453de3f356d761e13cc
SeanShubin <sshubin@cj.com>
2014-11-04 09:37:59 -0800

    Updated react

--- gui/src/main/resources/serve-from-classpath/require-config.js
+++ gui/src/main/resources/serve-from-classpath/require-config.js
@@ -8,7 +8,7 @@ var require = {
         'handlebars': 'lib/handlebars',
         'qunit': 'lib/qunit-1.14.0',
         'sinon': 'lib/sinon-1.10.0',
-        'react': 'lib/react-with-addons-0.10.0',
+        'react': 'lib/react-with-addons',
         'superagent': 'lib/superagent-0.18.0',
         'q': 'lib/q',
         'text': 'lib/text'
