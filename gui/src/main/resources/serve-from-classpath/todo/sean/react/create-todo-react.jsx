fa601b6b939ee2d1d571c55c59fbc099c92ce6df
SeanShubin <sshubin@cj.com>
2014-11-10 09:58:43 -0800

    Worked on react version

--- gui/src/main/resources/serve-from-classpath/todo/sean/react/create-todo-react.jsx
+++ gui/src/main/resources/serve-from-classpath/todo/sean/react/create-todo-react.jsx
@@ -1,6 +1,6 @@
 define(['react'], function (React) {
     'use strict';
-    var createTodo, TodoAdd;
+    var createTodo, TodoAdd, TodoElement;
 
     TodoAdd = React.createClass({
         render: function () {
@@ -13,6 +13,16 @@ define(['react'], function (React) {
         }
     });
 
+    TodoElement = React.createClass({
+        render: function(){
+            return <li class="todo-entry">
+                <input class="todo-done" type="checkbox"/>
+                <label class="todo-name"></label>
+                <button class="todo-delete">Delete</button>
+            </li>;
+        }
+    });
+
     createTodo = function (options) {
         var mount, todoApp;
         mount = options.domNode;
@@ -22,3 +32,93 @@ define(['react'], function (React) {
 
     return createTodo;
 });
+
+/*
+define(['jquery',
+    'underscore',
+    'underscore.string'], function ($, _, _s) {
+    'use strict';
+    function createPrototypeTodoComponent(jsonOverHttp) {
+        var dom, addButtonPressed, keyPressed, addButton, userInput, list, appendTodoEntryToView, respondToTodoAdded,
+            respondToRefreshTodoEntries, respondToTodoDeleted, respondToTodoModified, updateTodoElementValues;
+        dom = $(
+                '<div>' +
+                '    <input class="user-input" type="text">' +
+                '    <button class="add-todo-entry-button">Add todo entry</button>' +
+                '    <ul class="todo-entries-list">' +
+                '    </ul>' +
+                '</div>');
+        addButton = dom.find('.add-todo-entry-button');
+        userInput = dom.find('.user-input');
+        list = dom.find('.todo-entries-list');
+        keyPressed = function (event) {
+            if (event.which === 13) {
+                addButtonPressed();
+            }
+        };
+        addButtonPressed = function () {
+            var toAdd, todo;
+            toAdd = _s.trim(userInput.val());
+            if (!_s.isBlank(toAdd)) {
+                userInput.val('');
+                userInput.focus();
+                todo = { name: toAdd, done: false };
+                jsonOverHttp({uri: 'db/todo-entry', method: 'POST', body: todo}).then(respondToTodoAdded);
+            }
+        };
+        appendTodoEntryToView = function (todoEntry) {
+            var todoElement, deleteEvent, doneEvent;
+            todoElement = $(
+                    '<li class="todo-entry">' +
+                    '    <input class="todo-done" type="checkbox">' +
+                    '    <label class="todo-name"></label>' +
+                    '    <button class="todo-delete">Delete</button>' +
+                    '</li>');
+            list.append(todoElement);
+            todoElement.addClass('todo-id-' + todoEntry.id);
+            updateTodoElementValues(todoEntry);
+            deleteEvent = function () {
+                jsonOverHttp({uri: 'db/todo-entry/' + todoEntry.id, method: 'DELETE'}).then(respondToTodoDeleted);
+            };
+            doneEvent = function () {
+                var checkedValue = todoElement.find('.todo-done').is(':checked');
+                jsonOverHttp({
+                    uri: 'db/todo-entry/' + todoEntry.id,
+                    method: 'PATCH',
+                    body: {done: checkedValue}
+                }).then(respondToTodoModified);
+            };
+            todoElement.find('.todo-delete').on('click', deleteEvent);
+            todoElement.find('.todo-done').on('change', doneEvent);
+        };
+        respondToTodoAdded = function (response) {
+            appendTodoEntryToView(response.body);
+        };
+        respondToRefreshTodoEntries = function (response) {
+            _.each(response.body, appendTodoEntryToView);
+        };
+        respondToTodoDeleted = function (response) {
+            dom.find('.todo-id-' + response.body.id).remove();
+        };
+        updateTodoElementValues = function (todoEntry) {
+            var todoElement = dom.find('.todo-id-' + todoEntry.id);
+            todoElement.find('.todo-name').text(todoEntry.name);
+            if (todoEntry.done) {
+                todoElement.find('.todo-done').attr('checked', 'checked');
+            } else {
+                todoElement.find('.todo-done').removeAttr('checked');
+            }
+        };
+        respondToTodoModified = function (response) {
+            updateTodoElementValues(response.body);
+        };
+        addButton.on('click', addButtonPressed);
+        userInput.on('keyup', keyPressed);
+        jsonOverHttp({uri: 'db/todo-entry', method: 'GET'}).then(respondToRefreshTodoEntries);
+        return dom;
+    }
+
+    return createPrototypeTodoComponent;
+});
+
+ */
